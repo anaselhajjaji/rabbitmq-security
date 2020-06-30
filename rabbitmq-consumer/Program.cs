fb6ef4c59ee2514f63248e114ea75261f39a56e4
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-06-30 16:14:58 -0400

    reorganize certs

--- rabbitmq-consumer/Program.cs
+++ rabbitmq-consumer/Program.cs
@@ -11,35 +11,48 @@ namespace rabbitmq_consumer
     {
         static async Task Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5671, UserName = "rabbitmq", Password = "rabbitmq" };
-            factory.Ssl.Enabled = true;
-            factory.Ssl.ServerName = "rabbitmq";
-            factory.Ssl.CertPath = "consumer.p12";
-            factory.Ssl.CertPassphrase = "Besiege27pin67stoic";
-            factory.Ssl.CertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true; // TODO, to be implemented.
-
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            while (true)
             {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                try
                 {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                     autoAck: true,
-                                     consumer: consumer);
+                    var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5671, UserName = "rabbitmq", Password = "rabbitmq" };
+                    factory.Ssl.Enabled = true;
+                    factory.Ssl.ServerName = "rabbitmq";
+                    factory.Ssl.CertPath = "consumer.p12";
+                    factory.Ssl.CertPassphrase = "Besiege27pin67stoic";
+                    factory.Ssl.CertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true; // TODO, to be implemented.
 
-                // Wait indefinitely
-                await Task.Delay(Timeout.Infinite);
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "hello",
+                                             durable: false,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body = ea.Body.ToArray();
+                            var message = Encoding.UTF8.GetString(body);
+                            Console.WriteLine(" [x] Received {0}", message);
+                        };
+                        channel.BasicConsume(queue: "hello",
+                                             autoAck: true,
+                                             consumer: consumer);
+
+                        // Wait indefinitely
+                        await Task.Delay(Timeout.Infinite);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Console.WriteLine("[x] Waiting {0} seconds...", 10);
+                await Task.Delay(TimeSpan.FromSeconds(10));
             }
         }
     }
--- rabbitmq-producer/Program.cs
+++ rabbitmq-producer/Program.cs
@@ -11,35 +11,44 @@ namespace rabbitmq_producer
         {
             await Task.Run(async () => {
                 var count = 1;
-                while(true)
+                while (true)
                 {
-                    Console.WriteLine("[x] Waiting {0} seconds...", 10);
-                    await Task.Delay(TimeSpan.FromSeconds(10));
-
-                    var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5671, UserName = "rabbitmq", Password = "rabbitmq" };
-                    factory.Ssl.Enabled = true;
-                    factory.Ssl.ServerName = "rabbitmq";
-                    factory.Ssl.CertPath = "producer.p12";
-                    factory.Ssl.CertPassphrase = "Besiege27pin67stoic";
-                    factory.Ssl.CertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true; // TODO, to be implemented.
-
-                    using (var connection = factory.CreateConnection())
-                    using (var channel = connection.CreateModel())
+                    try
                     {
-                        channel.QueueDeclare(queue: "hello",
-                                             durable: false,
-                                             exclusive: false,
-                                             autoDelete: false,
-                                             arguments: null);
 
-                        string message = $"Hello World number { count++ }";
-                        var body = Encoding.UTF8.GetBytes(message);
+                        Console.WriteLine("[x] Waiting {0} seconds...", 10);
+                        await Task.Delay(TimeSpan.FromSeconds(10));
 
-                        channel.BasicPublish(exchange: "",
-                                             routingKey: "hello",
-                                             basicProperties: null,
-                                             body: body);
-                        Console.WriteLine("[x] Sent {0}", message);
+                        var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5671, UserName = "rabbitmq", Password = "rabbitmq" };
+                        factory.Ssl.Enabled = true;
+                        factory.Ssl.ServerName = "rabbitmq";
+                        factory.Ssl.CertPath = "producer.p12";
+                        factory.Ssl.CertPassphrase = "Besiege27pin67stoic";
+                        factory.Ssl.CertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true; // TODO, to be implemented.
+
+                        using (var connection = factory.CreateConnection())
+                        using (var channel = connection.CreateModel())
+                        {
+                            channel.QueueDeclare(queue: "hello",
+                                                 durable: false,
+                                                 exclusive: false,
+                                                 autoDelete: false,
+                                                 arguments: null);
+
+                            string message = $"Hello World number { count++ }";
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchange: "",
+                                                 routingKey: "hello",
+                                                 basicProperties: null,
+                                                 body: body);
+                            Console.WriteLine("[x] Sent {0}", message);
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
                     }
                 }
             });
