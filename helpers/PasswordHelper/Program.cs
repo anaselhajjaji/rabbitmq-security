48d9e781d0ce05a7c161ed75e265a09f3b1a3214
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-07-03 15:46:51 -0400

    adds helper to generate rabbitmq users pwds

---
+++ helpers/PasswordHelper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PasswordHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please specify the password as argument.");
+                return;
+            }
+
+            var pwd = RabbitMqPasswordHelper.EncodePassword(args[0]);
+            Console.WriteLine(pwd);
+        }
+    }
+}
